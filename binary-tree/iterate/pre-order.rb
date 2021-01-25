2878aa4c587980ede6d501c8f899bb67ea73229d
Ryan-Willpower <surasak.c.work@gmail.com>
2021-01-25 13:38:26 +0700

    feat: add iterative pre-order traversal

--- binary-tree/iterate/pre-order.rb
+++ binary-tree/iterate/pre-order.rb
@@ -1,9 +1,36 @@
 class TreeNode
   attr_accessor :val, :left, :right
 
-  def initailize(val= 0, left = nil, right = nil)
+  def initialize(val= 0, left = nil, right = nil)
     @val = val
     @left = left
     @right = right
   end
+end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+  return [] if !root
+
+  stack = []
+  stack.push(root)
+
+  data = []
+
+  while stack.size > 0 do
+    node = stack.pop()
+
+    data.push(node.val)
+
+    if node.right
+      stack.push(node.right)
+    end
+
+    if node.left
+      stack.push(node.left)
+    end
+  end
+
+  return data
 end
